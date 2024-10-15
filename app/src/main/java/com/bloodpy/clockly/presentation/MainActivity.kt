171603b6f788f875f39485f5d38156f1b0fb99f8
Gleb <bloodspyy@yandex.ru>
2024-10-15 10:21:44 +0400

    Add layers

--- app/src/main/java/com/bloodpy/clockly/presentation/MainActivity.kt
+++ app/src/main/java/com/bloodpy/clockly/presentation/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.bloodpy.clockly
+package com.bloodpy.clockly.presentation
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.bloodpy.clockly.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
