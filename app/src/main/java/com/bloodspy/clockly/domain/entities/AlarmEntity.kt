757a5db6bfd7cf33d1fdd0ebb918cee4ee5ea5bc
BloodSpyy <hahaclassik@gmail.com>
2024-10-18 15:23:56 +0400

    Set AlarmEntity parcelable

--- app/src/main/java/com/bloodspy/clockly/domain/entities/AlarmEntity.kt
+++ app/src/main/java/com/bloodspy/clockly/domain/entities/AlarmEntity.kt
@@ -1,9 +1,12 @@
 package com.bloodspy.clockly.domain.entities
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 import javax.inject.Inject
 
+@Parcelize
 data class AlarmEntity @Inject constructor(
     val id: Int,
     val alarmTime: Long,
     val isActive: Boolean
-)
+) : Parcelable
