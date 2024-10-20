f6ebe8548a8e075fcecc5304b62354e33bc09fd3
BloodSpyy <hahaclassik@gmail.com>
2024-10-20 20:34:18 +0400

    Made default value for isActive and alarmTime type is long

--- app/src/main/java/com/bloodspy/clockly/domain/entities/AlarmEntity.kt
+++ app/src/main/java/com/bloodspy/clockly/domain/entities/AlarmEntity.kt
@@ -4,6 +4,6 @@ import javax.inject.Inject
 
 data class AlarmEntity @Inject constructor(
     var id: Int? = null,
-    val alarmTime: String,
-    val isActive: Boolean,
+    val alarmTime: Long,
+    val isActive: Boolean = true,
 )
