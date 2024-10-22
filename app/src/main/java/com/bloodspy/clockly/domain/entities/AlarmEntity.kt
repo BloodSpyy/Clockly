2d704f235f44719d979e87578a666644f7432219
BloodSpyy <hahaclassik@gmail.com>
2024-10-22 10:34:55 +0400

    Set UNDEFINED_ID in AlarmEntity public

--- app/src/main/java/com/bloodspy/clockly/domain/entities/AlarmEntity.kt
+++ app/src/main/java/com/bloodspy/clockly/domain/entities/AlarmEntity.kt
@@ -3,12 +3,11 @@ package com.bloodspy.clockly.domain.entities
 import javax.inject.Inject
 
 data class AlarmEntity @Inject constructor(
-    //todo подумай, тут нужен val или var
     val id: Int = UNDEFINED_ID,
     val alarmTime: Long,
     val isActive: Boolean = true,
 ) {
     companion object {
-        private const val UNDEFINED_ID = 0
+        const val UNDEFINED_ID = 0
     }
 }
