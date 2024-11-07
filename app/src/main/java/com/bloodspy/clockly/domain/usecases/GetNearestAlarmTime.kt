230289792c87118667563ccd1153e22e5ece8b9f
BloodSpyy <hahaclassik@gmail.com>
2024-11-07 12:08:45 +0400

    Change long Flow<Long> in domain

--- app/src/main/java/com/bloodspy/clockly/domain/usecases/GetNearestAlarmTime.kt
+++ app/src/main/java/com/bloodspy/clockly/domain/usecases/GetNearestAlarmTime.kt
@@ -1,10 +1,11 @@
 package com.bloodspy.clockly.domain.usecases
 
 import com.bloodspy.clockly.domain.repositories.AlarmRepository
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class GetNearestAlarmTime @Inject constructor(
     private val alarmRepository: AlarmRepository,
 ) {
-    suspend operator fun invoke(): Long? = alarmRepository.getNearestAlarmTime()
+    suspend operator fun invoke(): Flow<Long?> = alarmRepository.getNearestAlarmTime()
 }
