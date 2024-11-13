71fd2178cd73e118fd45220fd5a5a01307fdedfe
BloodSpyy <hahaclassik@gmail.com>
2024-11-13 21:33:03 +0400

    Add audio attributes to RingtoneMediaPlayerHelper

--- app/src/main/java/com/bloodspy/clockly/helpers/RingtoneMediaPlayerHelper.kt
+++ app/src/main/java/com/bloodspy/clockly/helpers/RingtoneMediaPlayerHelper.kt
@@ -1,15 +1,25 @@
 package com.bloodspy.clockly.helpers
 
 import android.content.Context
+import android.media.AudioAttributes
 import android.media.MediaPlayer
 import android.media.RingtoneManager
 import android.net.Uri
 
 object RingtoneMediaPlayerHelper {
-    fun createRingtoneMediaPlayer(context: Context): MediaPlayer = MediaPlayer.create(
-        context,
-        getRingtoneUri()
-    ).apply { isLooping = true }
+    fun createRingtoneMediaPlayer(context: Context): MediaPlayer {
+        val audioAttributes = AudioAttributes.Builder()
+            .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+            .setUsage(AudioAttributes.USAGE_ALARM)
+            .build()
+
+        return MediaPlayer().apply {
+            setAudioAttributes(audioAttributes)
+            setDataSource(context, getRingtoneUri())
+            isLooping = true
+            prepare()
+        }
+    }
 
     private fun getRingtoneUri(): Uri {
         return RingtoneManager.getDefaultUri(
